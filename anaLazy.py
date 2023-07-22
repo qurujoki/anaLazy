b193983e0f452cadfc6723964ac80db8f1e303d4
qurujoki <88145365+qurujoki@users.noreply.github.com>
2023-07-22 18:32:17 +0300

    Revamp

--- anaLazy.py
+++ anaLazy.py
@@ -5,8 +5,31 @@ import json
 import os
 import readline
 import sys
-from typing import Any
+from datetime import datetime
+from typing import Any, Optional
 from urllib.parse import urlencode, urlparse
+
+ABUSEIPDB_API_KEY = os.environ.get("ABUSEIPDB", "")
+VIRUSTOTAL_API_KEY = os.environ.get("VIRUSTOTAL", "")
+
+HELP = """
+Usage:
+    anaLazy > COMMAND VALUE [OPTIONS]
+
+Commands:
+    domain          Check existing scan results for given domain.
+    hash            Check existing scan results for given hash.
+    ip              Check existing scan results for given IP.
+    url             Check existing scan results for given URL.
+    help            Print this message.
+    exit/quit/q     Exit the program. CTRL-D and CTRL-C also exit.
+
+Options:
+    -r, --raw       Print results of a command in raw JSON format.
+
+Other:
+    Command history is also available via `readline` during runtime. Press up/down arrows to circle through the command history.
+"""
 
 LOGO = """
  ▄▄▄       ███▄    █  ▄▄▄       ██▓    ▄▄▄      ▒███████▒▓██   ██▓
@@ -21,25 +44,28 @@ LOGO = """
                                                 ░         ░ ░     
 """
 
-EXIT_COMMANDS = ("exit", "quit", "q")
-
-ABUSEIPDB_API_KEY = os.environ.get("ABUSEIPDB", "")
-VIRUSTOTAL_API_KEY = os.environ.get("VIRUSTOTAL", "")
+print_raw = False
 
 
-def get_user_input() -> tuple[str, str]:
+def get_user_input() -> tuple[str, str, Optional[str | None]]:
     try:
         user_input = input("anaLazy > ")
         readline.add_history(user_input)
-        user_input = user_input.split(maxsplit=1) or [""]
+        user_input = user_input.split() or [""]
         _command = user_input[0].lower()
         _value = user_input[1].lower()
-        return _command, _value
+        try:
+            options = user_input[2].lower()
+        except IndexError:
+            return (_command, _value, "")
+        return (_command, _value, options)
     except (EOFError, KeyboardInterrupt):
         sys.exit()
     except IndexError:
-        if _command in EXIT_COMMANDS:
+        if _command in ("exit", "quit", "q"):
             sys.exit()
+        elif _command == "help":
+            print(HELP)
         return get_user_input()
 
 
@@ -73,20 +99,19 @@ async def send_async_http_request(
     )
 
 
-async def abuseipdb(ip_address: str):
-    def filter_results(results: dict) -> dict[str, Any] | None:
-        data = results.get("data", {})
-        return {
-            "Link": f"https://www.abuseipdb.com/check/{data.get('ipAddress')}",
-            "Abuse confidence score": f"{data.get('abuseConfidenceScore')}/100",
-            "Domain": data.get("domain"),
-            "Hostnames": ", ".join(data.get("hostnames", [])),
-            "ISP": data.get("isp"),
-            "Country code": data.get("countryCode"),
-            "Usage type": data.get("usageType"),
-            "Tor node": str(data.get("isTor")),
-        }
+def print_result_summary(data: dict[str, Any]) -> None:
+    key_width = max(len(str(key)) for key in data.keys())
+    print(f"\n{data.pop('Link')}\n")
+    for key, value in data.items():
+        key = key.ljust(key_width)
+        if isinstance(value, (list, set)):
+            value = ", ".join(str(i) for i in value)
+        if value and not isinstance(value, dict):
+            print(f"\x20\x20{key} : {str(value)}")
+    print()
 
+
+async def abuseipdb(ip_address: str) -> None:
     results = await send_async_http_request(
         method="GET",
         host="api.abuseipdb.com",
@@ -94,21 +119,157 @@ async def abuseipdb(ip_address: str):
         headers={"Accept": "application/json", "Key": ABUSEIPDB_API_KEY},
         parameters={"ipAddress": ip_address},
     )
-    if results:
-        return filter_results(results)
+    if results and print_raw:
+        print(f"\n{json.dumps(results)}\n")
+    else:
+        data = results.get("data", {})
+        print_result_summary(
+            {
+                "Link": f"https://www.abuseipdb.com/check/{data.get('ipAddress')}",
+                "Last reported": data.get("lastReportedAt"),
+                "Abuse confidence score": f"{data.get('abuseConfidenceScore')}/100",
+                "Domain": data.get("domain"),
+                "Hostnames": data.get("hostnames"),
+                "ISP": data.get("isp"),
+                "Country code": data.get("countryCode"),
+                "Usage type": data.get("usageType"),
+                "Tor node": data.get("isTor"),
+            }
+        )
 
 
-async def virustotal(object_type: str, object_identifier: str):
-    def filter_results(results: dict) -> dict[str, Any] | None:
+async def shodan(
+    search_value: str | None,
+    search_method: str = "/shodan/host/",
+) -> None:
+    results = await send_async_http_request(
+        method="GET",
+        host="api.shodan.io",
+        url=f"{search_method}{search_value}",
+    )
+    if results and print_raw:
+        print(f"\n{json.dumps(results)}\n")
+    else:
+        print_result_summary(
+            {
+                "Link": f"https://shodan.io/host/{results.get('ip_str')}",
+                "Last udpdated": results.get("last_update"),
+                "Tags": results.get("tags"),
+                "ISP": results.get("isp"),
+                "Ports": results.get("ports"),
+                "Hostnames": results.get("hostnames"),
+                "Country": results.get("country_name"),
+                "Domains": results.get("domains"),
+                "Organization": results.get("org"),
+            }
+        )
+
+
+async def urlscan(url: str) -> None:
+    def parse_domain(url: str) -> str:
+        parsed_url = urlparse(url)
+        if parsed_url.netloc:
+            return parsed_url.netloc
+        return parsed_url.path.split("/")[0].replace("www", "")
+
+    async def get_searches_for_domain(domain: str) -> dict:
+        return await send_async_http_request(
+            method="GET",
+            host="urlscan.io",
+            url="/api/v1/search/",
+            parameters={"q": f"domain:{domain}"},
+        )
+
+    async def get_search_results(result_id: str) -> dict:
+        return await send_async_http_request(
+            method="GET",
+            host="urlscan.io",
+            url=f"/api/v1/result/{result_id}",
+        )
+
+    related_searches = await get_searches_for_domain(parse_domain(url))
+    if related_searches:
+        for result in related_searches.get("results", {}):
+            results_available_for_url = url in result.get("task", {}).get("url", "")
+            if results_available_for_url:
+                search_result = await get_search_results(result.get("_id"))
+
+    if search_result and print_raw:
+        print(f"\n{json.dumps(search_result)}\n")
+    elif search_result:
+        print_result_summary(
+            {
+                "Link": search_result.get("task", {}).get("reportURL"),
+                "Submission": search_result.get("task", {}).get("time"),
+                "Score": f'{search_result.get("verdicts", {}).get("overall").get("score")}/100',
+                "Malicious": search_result.get("verdicts", {})
+                .get("overall")
+                .get("malicious"),
+                "Effective URL": search_result.get("page", {}).get("url"),
+                "Categories": search_result.get("verdicts", {})
+                .get("overall", {})
+                .get("categories"),
+                "Brands": search_result.get("verdicts", {})
+                .get("overall", {})
+                .get("brands"),
+                "Tags": search_result.get("verdicts", {})
+                .get("overall", {})
+                .get("tags"),
+                "Server": search_result.get("page", {}).get("server"),
+                "IP": search_result.get("page", {}).get("ip"),
+                "ASN name": search_result.get("page", {}).get("asnname"),
+            }
+        )
+
+
+async def virustotal(object_type: str, object_identifier: str) -> None:
+    def generate_link(object_type: str, object_id: str) -> str:
+        object_type_map = {
+            "domain": "domain",
+            "file": "hash",
+            "ip_address": "ip-address",
+            "url": "url",
+        }
+        return (
+            f"https://virustotal.com/gui/{object_type_map.get(object_type)}/{object_id}"
+        )
+
+    def sum_malicious_verdicts(last_analysis_results: dict[dict[Any, str]]) -> int:
+        return len(
+            [
+                verdict
+                for provider in last_analysis_results.keys()
+                for verdict in last_analysis_results[provider].values()
+                if verdict == "malicious"
+            ]
+        )
+
+    if object_type == "urls":
+        object_identifier = (
+            base64.urlsafe_b64encode(object_identifier.encode()).decode().strip("=")
+        )
+
+    results = await send_async_http_request(
+        method="GET",
+        host="virustotal.com",
+        url=f"/api/v3/{object_type}/{object_identifier}",
+        headers={"x-apikey": VIRUSTOTAL_API_KEY},
+    )
+    if results and print_raw:
+        print(f"\n{json.dumps(results)}\n")
+    else:
         data = results.get("data", {})
         attributes = data.get("attributes", {})
         last_analysis_results = attributes.get("last_analysis_results", {})
         filtered_result = {
             "Link": generate_link(data.get("type"), data.get("id")),
+            "Last analysis": datetime.fromtimestamp(
+                attributes.get("last_analysis_date")
+            ),
             "Malicious score (vendors)": f"{sum_malicious_verdicts(last_analysis_results)}/{len(last_analysis_results)}",
             "Malicious score (community)": f"{attributes.get('total_votes', {}).get('malicious')}/{sum(attributes.get('total_votes', {}).values())}",
-            "Tags": ", ".join(attributes.get("tags", [])),
-            "Categories": ", ".join(set(attributes.get("categories", {}).values())),
+            "Tags": attributes.get("tags"),
+            "Categories": set(attributes.get("categories", {}).values()),
         }
         if data.get("type") == "domain":
             filtered_result.update(
@@ -150,150 +311,31 @@ async def virustotal(object_type: str, object_identifier: str):
                     "Title": attributes.get("title"),
                 }
             )
-        return filtered_result
-
-    def generate_link(object_type: str, object_id: str) -> str:
-        object_type_map = {
-            "domain": "domain",
-            "file": "hash",
-            "ip_address": "ip-address",
-            "url": "url",
-        }
-        return (
-            f"https://virustotal.com/gui/{object_type_map.get(object_type)}/{object_id}"
-        )
-
-    def sum_malicious_verdicts(last_analysis_results: dict) -> int:
-        return len(
-            [
-                verdict
-                for provider in last_analysis_results.keys()
-                for verdict in last_analysis_results[provider].values()
-                if verdict == "malicious"
-            ]
-        )
-
-    if object_type == "urls":
-        object_identifier = (
-            base64.urlsafe_b64encode(object_identifier.encode()).decode().strip("=")
-        )
-
-    results = await send_async_http_request(
-        method="GET",
-        host="virustotal.com",
-        url=f"/api/v3/{object_type}/{object_identifier}",
-        headers={"x-apikey": VIRUSTOTAL_API_KEY},
-    )
-    if results:
-        return filter_results(results)
+        print_result_summary(filtered_result)
 
 
-async def shodan(search_method: str = "/shodan/host/", search_value: str = ""):
-    def filter_results(results: dict) -> dict[str, Any] | None:
-        return {
-            "Link": f"https://shodan.io/host/{results.get('ip_str')}",
-            "Tags": ", ".join(results.get("tags", [])),
-            "ISP": results.get("isp"),
-            "Ports": ", ".join(str(port) for port in results.get("ports", [])),
-            "Hostnames": ", ".join(results.get("hostnames", [])),
-            "Country": results.get("country_name"),
-            "Domains": ", ".join(results.get("domains", [])),
-            "Organization": results.get("org"),
-        }
-
-    results = await send_async_http_request(
-        method="GET",
-        host="api.shodan.io",
-        url=f"{search_method}{search_value}",
-    )
-    if results:
-        return filter_results(results)
-
-
-async def urlscan(url: str) -> dict[str, Any] | None:
-    def parse_domain(url: str) -> str:
-        parsed_url = urlparse(url)
-        if parsed_url.netloc:
-            return parsed_url.netloc
-        return parsed_url.path.split("/")[0].replace("www", "")
-
-    async def get_searches_for_domain(domain: str) -> dict:
-        return await send_async_http_request(
-            method="GET",
-            host="urlscan.io",
-            url="/api/v1/search/",
-            parameters={"q": f"domain:{domain}"},
-        )
-
-    async def get_search_results(result_id: str) -> dict:
-        return await send_async_http_request(
-            method="GET",
-            host="urlscan.io",
-            url=f"/api/v1/result/{result_id}",
-        )
-
-    def filter_results(results: dict) -> dict[str, Any]:
-        return {
-            "Link": results.get("task", {}).get("reportURL"),
-            "Effective URL": results.get("page", {}).get("url"),
-            "Score": f'{results.get("verdicts", {}).get("overall").get("score")}/100',
-            "Malicious": results.get("verdicts", {}).get("overall").get("malicious"),
-            "Categories": ", ".join(
-                results.get("verdicts", {}).get("overall", {}).get("categories")
-            ),
-            "Brands": ", ".join(
-                results.get("verdicts", {}).get("overall", {}).get("brands")
-            ),
-            "Tags": ", ".join(
-                results.get("verdicts", {}).get("overall", {}).get("tags")
-            ),
-            "Server": results.get("page", {}).get("server"),
-            "IP": results.get("page", {}).get("ip"),
-            "ASN name": results.get("page", {}).get("asnname"),
-        }
-
-    related_searches = await get_searches_for_domain(parse_domain(url))
-    if related_searches:
-        for result in related_searches.get("results", {}):
-            results_available_for_url = url in result.get("task", {}).get("url", "")
-            if results_available_for_url:
-                return filter_results(await get_search_results(result.get("_id")))
-
-
-def print_table(data: dict) -> None:
-    key_width = max(len(str(key)) for key in data.keys())
-    print(f"\n{data.pop('Link')}\n")
-    for key, value in data.items():
-        key = str(key).ljust(key_width)
-        value = str(value)
-        if value:
-            print(f"  {key} : {value}")
-
-
-async def main():
+async def main() -> None:
+    global print_raw
     while True:
-        _command, _value = get_user_input()
+        _command, _value, options = get_user_input()
+
+        if "-r" in options:
+            print_raw = True
+
         tasks = []
+        if _command == "domain":
+            tasks.extend([virustotal("domains", _value)])
+        if _command == "hash":
+            tasks.extend([virustotal("files", _value)])
         if _command == "ip":
             tasks.extend(
-                [
-                    abuseipdb(_value),
-                    virustotal("ip_addresses", _value),
-                    shodan(search_value=_value),
-                ]
+                [abuseipdb(_value), shodan(_value), virustotal("ip_addresses", _value)]
             )
-        elif _command == "domain":
-            tasks.extend([virustotal("domains", _value)])
-        elif _command == "url":
-            tasks.extend([virustotal("urls", _value), urlscan(_value)])
-        elif _command == "hash":
-            tasks.extend([virustotal("files", _value)])
+        if _command == "url":
+            tasks.extend([urlscan(_value), virustotal("urls", _value)])
 
-        for task in asyncio.as_completed(tasks):
-            results = await task
-            if results:
-                print_table(results)
-        print()
+        await asyncio.gather(*tasks)
+        print_raw = False
 
 
 if __name__ == "__main__":
